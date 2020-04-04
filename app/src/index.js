546dbdcfd3837c4c8e4192853c54823c2d401019
kronchusa <david@kronchusa.com>
2020-04-04 23:52:38 +0000

    Adding Redux Environ, User Storage

--- app/src/index.js
+++ app/src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from "react-redux"
+import {createStore} from "redux"
+
+const store = createStore()
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
