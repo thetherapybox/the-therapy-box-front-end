9800884e5b02ad82728026432d4ff6e0ed4927f2
kronchusa <david@kronchusa.com>
2020-04-05 01:10:56 +0000

    User Authentication System

--- app/src/components/login.js
+++ app/src/components/login.js
@@ -1,16 +1,31 @@
 import React from "react"
 import {TextField, Button} from "@material-ui/core"
+import {Alert} from "@material-ui/lab"
+import Strapi from "../strapi"
+
+import setUserTokenAction from "../actions/user"
+
+import {useSelector, useDispatch} from "react-redux"
 
 export default function Login(){
+
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.user)
 
     const textFieldStyle = {style: {color: '#fff', margin: '5px'}}
 
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
+    const [authenticationError, setAuthenticationError] = React.useState('')
+
+    const showError = authenticationError != ''
+    const userAuthenticated = user.token != undefined
 
     return (
         <div>
             <p>Test Login for The Therapy Box</p>
+            {userAuthenticated ? <Alert severity="success" style={{margin: '20px'}}>Login Successful</Alert> : ""}
+            {showError ? <Alert severity="error" style={{margin: '20px'}}>{authenticationError}</Alert> : ""}
             <TextField
                 variant={"filled"}
                 label={"Email"}
@@ -34,6 +49,19 @@ export default function Login(){
                 variant={'contained'}
                 color={'primary'}
                 style={{margin: '30px'}}
+                onClick={ev => {
+                    Strapi.login(email, password).then(res => {
+                        dispatch(setUserTokenAction(undefined))
+                        if(res.statusCode===400) 
+                            setAuthenticationError('Invalid Username and/or Password')
+                        else if(res.jwt === undefined)
+                            setAuthenticationError('Invalid Token Received.  Please Reload.')
+                        else {
+                            setAuthenticationError('')
+                            dispatch(setUserTokenAction(res.jwt))
+                        }
+                    })
+                }}
             >Login
             </Button>
         </div>
--- app/src/reducers/user.js
+++ app/src/reducers/user.js
@@ -1,6 +1,8 @@
+import {SET_USER_TOKEN} from "../actions/user"
 
 const userReducer = (state={}, action) => {
     switch(action.type){
+        case SET_USER_TOKEN: return {...state, token: action.token}
         default: return {...state}
     }
 }
