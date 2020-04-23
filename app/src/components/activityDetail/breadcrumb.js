1166bd2a290a0dc1e3b721b01cbf40930adb641d
kronchusa <david@kronchusa.com>
2020-04-23 03:17:32 +0000

    Activity Detail Page  Fleshed out on Example data

--- app/src/components/activityDetail/breadcrumb.js
+++ app/src/components/activityDetail/breadcrumb.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 import {Breadcrumbs, Typography} from "@material-ui/core"
-export default function Breadcrumb() {
+export default function Breadcrumb({id}) {
     return (
         <Breadcrumbs>
             <Typography color="inherit">Home</Typography>
             <Typography color="inherit">Activity Library</Typography>
-            <Typography color="textPrimary">Activity Box #01</Typography>
+            <Typography color="textPrimary">Activity Box #{id}</Typography>
         </Breadcrumbs>
     )
 }
