e0254f624c8537a15eda0b9d2fe8f7f54c18c53d
kronchusa <david@kronchusa.com>
2020-05-10 03:00:11 +0000

    Activity Box Integration GraphQL

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import {useDispatch} from "react-redux"
 
+import './App.css';
 import PageLayout from "./layouts/page"
+import {FetchActivityBoxes} from "./actions/activityboxes"
+
+import backendURL from "./backend"
+
+const bURL = backendURL.backendURL
+
+ 
+async function importGraphQLFromStrapi(url) {
+
+    const fetchRequest = await fetch(url, {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({
+                query: `
+                    query {
+                        activityboxes {
+                            id
+                            title
+                            price
+                            duration_days
+                            description
+                            photos {
+                                url
+                            }
+                            tags {
+                              name  
+                            }
+                        }
+                    }`
+            })
+        })
+    const data = await fetchRequest.json()
+    return data
+}
 
 function App() {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const url = `${bURL}/graphql`
+        importGraphQLFromStrapi(url).then(data => dispatch(FetchActivityBoxes(data.data.activityboxes)))
+    }, [dispatch])
 
   return (
     <div className="App">
---
+++ app/src/actions/activityboxes.js
@@ -0,0 +1,6 @@
+export const FETCH_BOXES = "FETCH_BOXES"
+
+export const FetchActivityBoxes = (payload) => ({
+    type: FETCH_BOXES,
+    payload: payload
+})
